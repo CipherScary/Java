2635d195fbed4a89b81bde584d38caf217d08043
Sebastião Marques <141269254+CipherScary@users.noreply.github.com>
2023-10-06 15:01:40 -0300

    Update SequênciaDeFibonacci.java

--- ATVJava Danilo Alves e Sebastião/SequênciaDeFibonacci.java
+++ ATVJava Danilo Alves e Sebastião/SequênciaDeFibonacci.java
@@ -2,15 +2,16 @@ package ATVJava;
 
 public class SequênciaDeFibonacci {
     public static void main(String[] args){
-        int num1 = 1;
+        int num1 = 1; 
         int num2 = 0;
 
-       System.out.println(num2); 
-       System.out.println(num1); 
-        for(int i = 0; i < 8;i++){
-            num1 = num1 + num2;
-            num2 = num1 - num2;
-            System.out.println(num1);
+        System.out.println(num2);  // Imprime o primeiro número 0.
+        System.out.println(num1);  // Imprime o segundo número 1.
+        
+        for(int i = 0; i < 8; i++){  // Loop para gerar os próximos 8 números na sequência de Fibonacci.
+            num1 = num1 + num2;  // Calcula o próximo número na sequência somando os dois números anteriores.
+            num2 = num1 - num2;  // Atualiza o segundo número para o valor anterior de num1.
+            System.out.println(num1);  // Imprime a sequência de Fibonacci.
         }
     }
 }
